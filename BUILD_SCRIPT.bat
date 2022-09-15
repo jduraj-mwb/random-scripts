c1b4111425265bc09741287b26b6d01c624e1750
Julien Duraj <jduraj@malwarebytes.com>
2022-09-15 14:35:17 +0300

    Added build args

--- BUILD_SCRIPT.bat
+++ BUILD_SCRIPT.bat
@@ -1,4 +1,4 @@
 set msbuild_exe="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
 
-echo %msbuild_exe%
-%msbuild_exe%
+%msbuild_exe% src\AdwCleaner\AdwCleanerGUI.sln /p:Configuration=Release /p:Platform=x86 /nr:false /m /verbosity:normal
+%msbuild_exe% src\AdwCleaner\AdwCleanerGUI.sln /p:Configuration=ReleaseNKNR /p:Platform=x86 /nr:false  /m /verbosity:normal
